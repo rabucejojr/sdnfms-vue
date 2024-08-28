878ba7cfbc5130a098bb95c833744656a502a0d6
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-08-28 17:38:14 +0800

    ongoing dev

--- src/components/TextInputs.vue
+++ src/components/TextInputs.vue
@@ -1,11 +1,15 @@
 <template>
     <div v-for="(tag, index) in tags" :key="tag">
-        {{ index + ' : ' + tag }}
+        {{ tag }}
+        <a @click.prevent="removeTag(index)" href="#">&times;</a>
     </div>
-    <div>
-        {{ tags.length }}
-    </div>
-    <input type="text" :value="newTag" v-on:keydown.enter="tags.push($event.target.value  )">
+    <input type="text" 
+    v-model.trim="newTag"
+    @keydown.enter="addNewTag"
+    @keydown.delete="removeLastTag"
+    @keydown.tab.prevent="addNewTag"
+    :class="{'tags-exists' : isTagExists}"
+    >
 </template>
 
 <script>
@@ -13,8 +17,41 @@ export default {
     data() {
         return {
             tags: ["vue","react","angular"],
-            newTag:"preact"
+            newTag:""
         }
     },
+    watch:{
+        newTag(newVal,oldVal){
+            console.log(newVal,oldVal)
+        }
+    },
+    computed:{
+        isTagExists(){
+           return this.tags.includes(this.newTag)
+        }
+    },
+    methods:{
+        addNewTag(){
+            if(this.newTag && !this.isTagExists){
+                this.tags.push(this.newTag);
+                this.newTag = ""
+            }
+        },
+        removeTag(index){
+            this.tags.splice(index,1);
+        },
+        removeLastTag(){
+            if(this.newTag.length ===0){
+                this.removeTag(this.tags.length -1)
+            }
+        }
+    }
 }
 </script>
+
+<style scoped>
+.tags-exists{
+    color:red;
+    text-decoration:line-through;
+}
+</style>
